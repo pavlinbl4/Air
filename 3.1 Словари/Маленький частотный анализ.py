e0d9dd0dcc672addd992eff0602b5d83189fbdfc
evgeniy <pavlinbl4@gmail.com>
2020-07-05 23:29:22 +0300

    вариант чужого решения для поиска букв

--- 3.1 Словари/Маленький частотный анализ.py
+++ 3.1 Словари/Маленький частотный анализ.py
@@ -2,14 +2,14 @@
 и количество его появлений через пробел. Гарантируется, что из знаков препинания встречаются только символы
 {",", ".", "?", "!"}, а также то, что в тексте есть только одно слово с наибольшей частотой появлений."""
 
-# text = input()
-text = "Корабли лавировали, лавировали, да не вылавировали"
-text = text.lower()
-text = text.replace(",","")
-text = text.replace(".","")
-text = text.replace("?","")
-text = text.replace("!","")
-text = text.split(" ")
+text = input()
+# text = "Buffalo buffalo Buffalo buffalo buffalo buffalo Buffalo buffalo"
+text = text.lower().replace(",","").replace(".","").replace("?","").replace("!","").split(" ")
+# text = text.replace(",","")
+# text = text.replace(".","")
+# text = text.replace("?","")
+# text = text.replace("!","")
+# text = text.split(" ")
 # n = len(text)
 # print(text, n, type(text))
 # for x in text():
@@ -22,3 +22,4 @@ dct = {}
 for x in text:
     dct[x] = dct.get(x,0) +1
 m = max(dct.values())
+print(*[ key for key, val in dct.items() if val == m],m)
