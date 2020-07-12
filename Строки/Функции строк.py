8d16710ca50077380e65243c84fb92646b5c67ce
evgeniy <pavlinbl4@gmail.com>
2020-07-12 19:15:49 +0300

    закрепляем и проверяем функции строк

--- Строки/Функции строк.py
+++ Строки/Функции строк.py
@@ -1,3 +1,8 @@
-s ="фуфловая фура обогнала фургон"
+s ="   фуфловая фура обогнала фургон"
 print(type(s))
 print(s.capitalize())
+print("состоит строка из букв ? - ", s.isalpha())
+# s1=s.replace(" ","")
+# print("состоит строка из букв, не учитываем пробелы ? - ", s1.isalpha())
+print(s.replace(" ","").isalpha())
+print(s.strip())
