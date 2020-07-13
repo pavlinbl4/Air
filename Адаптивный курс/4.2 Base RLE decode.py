29f0de792d46c3da120303f9da2f1cb19aabe66c
evgeniy <pavlinbl4@gmail.com>
2020-07-13 11:03:30 +0300

    не смотря на то, что на тестовое задание ответ правильный, решение не принимается

---
+++ Адаптивный курс/4.2 Base RLE decode.py
@@ -0,0 +1,11 @@
+# d = "3ab4c2CaB"
+d = input()
+l = [x for x in d]
+rez = []
+for x in range(len(l)):
+    if l[x].isdigit() == True:
+        rez.append((int(l[x])-1)*(l[x+1]))
+    else:
+        rez.append(l[x])
+print("".join(rez))
+
