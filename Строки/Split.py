b5730d99c0e6a11f28e6a7e66d2adfbb9abff296
evgeniy <pavlinbl4@gmail.com>
2020-07-17 23:28:02 +0300

    как же мне создать словарь по прочтенным книгам

--- Строки/Split.py
+++ Строки/Split.py
@@ -7,8 +7,31 @@ print(s)
 print()
 print()
 
+
+dict={}
 b = 'Dontsova "Десять негритят"'
+a = ['Dontsova', '10 негритят']
 print(b)
 b  = b.split(' "')
 b[1] = b[1][:-1]
 print(b)
+dict=  {b[0]}
+# dict[b[b]].append(5555)
+# dict = {a[0]:0[a[1]]}
+print(dict)
+print(dict[Dontsova])
+
+
+
+# book =  input().split(", ")
+# lybr = {x: book[x] for x in range(len(book))}
+# print(lybr)
+# print(len(lybr[0]))
+# print(book)
+# lybr = {}
+# for x in range(len(book)):
+#
+#     book[x] = book[x].split(' (')
+#     book[x][1] = book[x][1][:-1]
+#     lybr.update([book[x]])
+# print(lybr)
