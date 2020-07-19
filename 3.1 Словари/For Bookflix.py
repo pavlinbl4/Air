484b2e5233a476f75d086f69427139babdd5ffd2
evgeniy <pavlinbl4@gmail.com>
2020-07-20 00:13:01 +0300

    пытаюсь преобразовать словарь библиотеки удалив авторов

---
+++ 3.1 Словари/For Bookflix.py
@@ -0,0 +1,14 @@
+# создадим словарь библиотеки
+book =  input().split(", ")
+lybr = {}
+for x in range(len(book)):
+    book[x] = book[x].split(' (')
+    book[x][1] = book[x][1][:-1]
+    book[x][0] = book[x][0].split(' "')
+    book[x][0][1] = book[x][0][1][:-1]
+    book[x][0][1] = str(book[x][0][1])
+    book[x][0].pop(0)
+    print(book[x][0])
+    print(book[x])
+    lybr.update([book[x]])
+print(lybr)
