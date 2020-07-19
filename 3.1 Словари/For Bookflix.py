b5b0074828811739ff90505611607b397bde512f
evgeniy <pavlinbl4@gmail.com>
2020-07-20 00:22:49 +0300

    вот оно создание спискак книг без авторов

--- 3.1 Словари/For Bookflix.py
+++ 3.1 Словари/For Bookflix.py
@@ -8,7 +8,8 @@ for x in range(len(book)):
     book[x][0][1] = book[x][0][1][:-1]
     book[x][0][1] = str(book[x][0][1])
     book[x][0].pop(0)
-    print(book[x][0])
-    print(book[x])
+    # print(book[x][0])
+    book[x][0] = "".join(book[x][0])
+    # print(book[x])
     lybr.update([book[x]])
 print(lybr)
