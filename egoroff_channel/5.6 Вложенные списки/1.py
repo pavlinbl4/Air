3d93c44ebc3471db720f59fa09b56b565492c8b0
evgeniy <pavlinbl4@gmail.com>
2020-08-05 21:38:29 +0300

    решено успешно

---
+++ egoroff_channel/5.6 Вложенные списки/1.py
@@ -0,0 +1,24 @@
+# a = []
+# sum = []
+# n = int(input())
+# m = n
+# for i in range(n):
+#     b = []
+#     for i in range(m):
+#         b.append(input())
+#
+#     a.append(b)
+# for i in a:
+#     print(i)
+
+a = []
+ss = []
+n = int(input())
+for i in range(n):
+
+    a.append(list(map(int, input().split())))
+for i in range(n):
+    for j in range(n):
+        if i == j:
+            ss.append(a[i][j])
+print(sum(ss))
