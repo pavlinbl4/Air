60ae8c32417b4f2ab396d458f76d10286f7d9fc1
evgeniy <pavlinbl4@gmail.com>
2020-08-06 22:13:39 +0300

    При таком варианте проверки Ben 89001234050, +7 050 432 10-09 Alice 404-502, 894053212-65, 439-095 Nick +1(650)781 12-51 Ben Alex +4(908)273-22-421, 8 (908) 273-22-42 Alice Nick Robert 51234047129, 89174043215 Alex Robert Ben 89817139743 Ben . последний номер бена добавляется как списко

--- 3.1 Словари/Телефонная книга - Этап 3/Фильтр для телефонной книги 3.py
+++ 3.1 Словари/Телефонная книга - Этап 3/Фильтр для телефонной книги 3.py
@@ -56,7 +56,6 @@ while True:
             phones = numb.split(",") # создаю список телефонов
             for x in range(len(phones)): # удаляю пробелы и тд в номерах
                 optimize()
-                # if (phones[x][0] == "+" and phones[x][1] != "7") or (len(phones[x]) == 12 and phones[x][0] != "+") or (len(phones[x]) == 11 and phones[x][0] != "8" or len(phones[x]) < 11 or len(phones[x]) > 12):
                 if (phones[x][0] == "+" and phones[x][1] != "7")  or (
                         len(phones[x]) == 11 and phones[x][0] != "8" or len(phones[x]) < 11 or len(phones[x]) > 12):
                     phones[x] = 0 # заменяю на ноль некорректный номер
