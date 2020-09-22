8ed620d7fb4c1edac85c8a8ba500929723aefdb5
pavlinbl4@gmail.com <15deaDman>
2020-09-22 11:55:06 +0300

    easy task

---
+++ 4.3 Вложенные и каскадные условия/Количество дней.py
@@ -0,0 +1,7 @@
+x = int(input())
+if x == 1 or x == 3 or x == 5 or x == 7 or x == 8 or x == 10 or x == 12:
+    print(31)
+elif x == 4 or x == 6 or x == 9 or x == 11:
+    print(30)
+else:
+    print(28)
