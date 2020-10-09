48d64995eacf6552e02a8b5535328da28b8f9960
pavlinbl4@gmail.com <15deaDman>
2020-10-09 16:40:38 +0300

    вроде все идет правильно

--- egoroff_channel/5.6 Вложенные списки/ской бой - 2(Air).py
+++ egoroff_channel/5.6 Вложенные списки/ской бой - 2(Air).py
@@ -10,6 +10,9 @@ for i in range(n):
 a.append(["@" for x in range(m + 2)])
 for i in a:
     print(i)
-for i in range(1,n):
-    for j in range(1,m):
-        print(a[i][j])
+for i in range(1,n+1):
+    # print()
+    for j in range(1,m+1):
+        # print(a[i][j],end =" ")
+        if a[i][j] == "*" and a[i-1][j-1] != "." and a[i-1][j] != "." and a[i][j-1] != "." and a[i+1][j] != "." and a[i][j+1] != "."
+            and a[+i][+j] != "."
